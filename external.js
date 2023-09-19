1b4e8aec6f23a8cfd1d0f3311980873ca1fb1f75
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2023-09-19 09:40:14 +0800

    Modified external.js to add playRound function

--- external.js
+++ external.js
@@ -2,6 +2,32 @@
 function getComputerChoice() {
     var handSigns = ['Rock', 'Paper', 'Scissor'];
     var handSign = handSigns[Math.floor(Math.random()*handSigns.length)]
-    return console.log(handSign);
+    return handSign;
 }
-getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie !!!");
+    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        console.log("You Win! Rock beats Scissor");
+    } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
+        console.log("You Lose! Scissor beats Paper");
+    } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
+        console.log("You Lose! Rock beats Scissor");
+    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+        console.log("You Win! Scissor beats Paper");
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("You Lose! Paper beats Rock");
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("You Win! Paper beats Rock");
+    } else {
+        console.log("Error");
+    }
+    
+    
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
